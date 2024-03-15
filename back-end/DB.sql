8169c3600270d03ceeb86ec38fc48d23d49d494a
Praveen <praveenanand333@gmail.com>
2024-03-15 20:55:02 +0530

    table-creation

--- back-end/DB.sql
+++ back-end/DB.sql
@@ -36,48 +36,53 @@ CREATE TABLE Students (
     FOREIGN KEY (FacultyAdvisorID) REFERENCES Teachers(TeacherID)
 );
 --personal and primary data ends
-
----created above    
-
+  
 --Other details table
 
 CREATE TABLE Internship (
-    roll_number INT PRIMARY KEY,
-    FOREIGN KEY (roll_number) REFERENCES Students(roll_number),
-    employer_details VARCHAR(255),
+    roll_number INT,
+    employer_name VARCHAR(255),
     on_off_campus ENUM('On Campus', 'Off Campus'),
     ctc DECIMAL(10, 2),
     InternshipDuration VARCHAR(50),
     InternshipStartDate DATE,
     InternshipEndDate DATE,
-    product_service_based ENUM('Product Based', 'Service Based')
+    product_service_based ENUM('Product Based', 'Service Based'),
+    PRIMARY KEY (roll_number, employer_name),
+    FOREIGN KEY (roll_number) REFERENCES Students(RollNumber)
 );
 
 CREATE TABLE Scholarship (
-    roll_number INT PRIMARY KEY,
-    FOREIGN KEY (roll_number) REFERENCES Students(roll_number),
-    name VARCHAR(255),
-    amount DECIMAL(10, 2)
+    roll_number INT,
+    ScholarshipProvider VARCHAR(255),
+    amount DECIMAL(10, 2),
+    PRIMARY KEY (roll_number, ScholarshipProvider),
+    FOREIGN KEY (roll_number) REFERENCES Students(RollNumber)
 );
 
 CREATE TABLE Project (
-    roll_number INT PRIMARY KEY,
-    FOREIGN KEY (roll_number) REFERENCES Students(roll_number),
+    roll_number INT,
     title VARCHAR(255),
-    guide VARCHAR(255)
+    guide VARCHAR(255),
+    project_desc VARCHAR(255),
+    PRIMARY KEY (roll_number, title),
+    FOREIGN KEY (roll_number) REFERENCES Students(RollNumber)
 );
 
+
 CREATE TABLE Sports (
-    roll_number INT PRIMARY KEY,
-    FOREIGN KEY (roll_number) REFERENCES Students(roll_number),
+    roll_number INT,
     event_name VARCHAR(255),
-    award VARCHAR(255)
+    award VARCHAR(255),
+    PRIMARY KEY (roll_number, event_name, award),
+    FOREIGN KEY (roll_number) REFERENCES Students(RollNumber)
 );
+
 
 
 CREATE TABLE Exams_Attended (
     roll_number INT PRIMARY KEY,
-    FOREIGN KEY (roll_number) REFERENCES Students(roll_number),
+    FOREIGN KEY (roll_number) REFERENCES Students(RollNumber),
     GATE_score DECIMAL(10, 2),
     GRE_score DECIMAL(10, 2),
     TOEFL_score DECIMAL(10, 2),
@@ -87,24 +92,29 @@ CREATE TABLE Exams_Attended (
 );
 
 CREATE TABLE Paper_Published (
-    roll_number INT PRIMARY KEY,
-    FOREIGN KEY (roll_number) REFERENCES Students(roll_number),
+    roll_number INT,
     title VARCHAR(255),
     journal VARCHAR(255),
     date_year DATE,
-    DOI_link VARCHAR(255)
+    DOI_link VARCHAR(255),
+    PRIMARY KEY (roll_number, title, date_year),
+    FOREIGN KEY (roll_number) REFERENCES Students(RollNumber)
 );
+
 
 CREATE TABLE Events (
-    roll_number INT PRIMARY KEY,
-    FOREIGN KEY (roll_number) REFERENCES Students(roll_number),
+    roll_number INT,
     event_name VARCHAR(255),
     institution VARCHAR(255),
+    date DATE,
     role ENUM('Participate', 'Conduct/Manage'),
-    awards VARCHAR(255)
+    awards VARCHAR(255),
+    PRIMARY KEY (roll_number, event_name, institution, date),
+    FOREIGN KEY (roll_number) REFERENCES Students(RollNumber)
 );
----other details end
 
+---other details end
+---created above  
 ---academic data
 
 CREATE TABLE Subjects (
